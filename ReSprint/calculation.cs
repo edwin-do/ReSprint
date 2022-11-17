1ef0996b86513e00bc422ab661f5da0741da861e
Timothy Chen <chent61@mcmaster.ca>
2022-11-17 17:10:32 -0500

    added class for calculations

---
+++ ReSprint/calculation.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReSprint
+{
+    class calculation
+    {
+        public calculation(){}
+
+        public double calcResistence(double voltage, double current)
+        {
+            return voltage/current;
+        }
+
+        public double calcResistivity(double resisitance, double area, double length)
+        {
+            return (resisitance*area)/length;
+        }
+    }
+}
