ca62e6f8f00373f04bf7d178cdd58643571ed4f8
Edwin Do <doe@mcmaster.ca>
2023-01-29 16:29:53 -0800

    restructure input and instrument layout + add current state module

---
+++ ReSprint/CurrentState.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace ReSprint
+{
+	class CurrentState
+	{
+		public CurrentState() {
+			string userName = "";
+			string date = "";
+			string fileName = "";
+            string sampleName = "";
+            float samplingRate = 1.0f;
+			float sampleLength = 1.0f;
+			float sampleWidth = 1.0f;
+
+        }
+
+/*		public displayUserInfo(string userName, string sampleName, string date, string fileName, float samplingRate, float sampleLength, float sampleWidth)
+		{
+
+		}*/
+
+		public updateUserInfo(string newUserInfo, int id)
+		{
+			switch(id)
+			{
+				case 0:
+					this.userName = newUserInfo;
+					break;
+				case 1:
+					this.date = newUserInfo;
+					break;
+				case 2:
+					this.fileName = newUserInfo;
+					break;
+				default: break;
+			}
+		}
+
+		public updateSampleInfo(string newSampleInfo, int id)
+		{
+            switch (id)
+            {
+                case 0:
+                    this.samplingRate = float.parse(newSampleInfo);
+                    break;
+                case 1:
+                    this.sampleName = newSampleInfo;
+                    break;
+                case 2:
+                    this.sampleWidth = float.parse(newSampleInfo);
+                    break;
+                case 3:
+                    this.sampleLength = float.parse(newSampleInfo);
+                    break;
+
+                default: break;
+            }
+        }
+
+/*		public displayHardwareState()
+		{
+
+		}*/
+	}
+}
