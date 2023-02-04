725131ef766f6b1433212335da30f54b2a5d294e
SirWolffe <braunj2@mcmaster.ca>
2023-02-04 16:19:59 -0500

    Added CurrentState, Calculation, and InputCommunication class files

---
+++ RealSprint/ChartDemo/InputCommunication.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RealSprint
+{
+    class InputCommunication
+    {
+        public InputCommunication() 
+        { 
+            voltage = 0.0;
+            current = 0.0;
+
+        }
+
+        public double GetVoltage()
+        {
+            //Returns voltage reading (in mV) from nanovoltmeter
+            voltage = 0.00356;
+            return voltage;
+        }
+
+        public double GetCurrent()
+        {
+            //Returns output value (in mA) from current source
+            current = 1.0;
+            return current;
+        }
+
+        public void EnableCurrent()
+        {
+            //enable current output on current source
+            //TODO: implement with SCPI commands
+            return;
+        }
+
+        public void DisableCurrent()
+        {
+            //disable current output on current source
+            //TODO: implement with SCPI commands
+            return;
+        }
+
+        //private member variables
+        private double voltage;
+        private double current;
+    }
+}
---
+++ RealSprint/ChartDemo/calculation.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReSprint
+{
+    class calculation
+    {
+        public calculation(){}
+
+        public double calcResistence(double voltage, double current)
+        {
+            return Math.Round(voltage/current, 3);
+        }
+
+        public double calcResistivity(double resisitance, double area, double length)
+        {
+            return Math.Round((resisitance*area)/length,3);
+        }
+    }
+}
