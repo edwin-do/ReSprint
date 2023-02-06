d143bcc7158c9a732abc2252a2e9898feb4e9d3a
unknown <seddikia@mcmaster.ca>
2023-02-05 20:23:19 -0500

    final sprint

---
+++ FinalSprint/CurrentState.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace FinalSprint
+{
+    class CurrentState
+    {
+        public CurrentState()
+        {
+            string userName = "";
+            string date = "";
+            string fileName = "";
+            string sampleName = "";
+            float samplingRate = 1.0f;
+            float sampleLength = 1.0f;
+            float sampleWidth = 1.0f;
+
+        }
+
+        /*		public displayUserInfo(string userName, string sampleName, string date, string fileName, float samplingRate, float sampleLength, float sampleWidth)
+                {
+
+                }*/
+
+        public void updateUserInfo(string newUserInfo, int id)
+        {
+            switch (id)
+            {
+                case 0:
+                    this.userName = newUserInfo;
+                    break;
+                case 1:
+                    this.date = newUserInfo;
+                    break;
+                case 2:
+                    this.fileName = newUserInfo;
+                    break;
+                default: break;
+            }
+        }
+
+        //public void updateSampleInfo(string newSampleInfo, int id)
+        //{
+        //    switch (id)
+        //    {
+        //        case 0:
+        //            this.samplingRate = float.parse(newSampleInfo);
+        //            break;
+        //        case 1:
+        //            this.sampleName = newSampleInfo;
+        //            break;
+        //        case 2:
+        //            this.sampleWidth = float.parse(newSampleInfo);
+        //            break;
+        //        case 3:
+        //            this.sampleLength = float.parse(newSampleInfo);
+        //            break;
+
+        //        default: break;
+        //    }
+        //      }
+
+        /*		public displayHardwareState()
+                {
+
+                }*/
+
+        string userName;
+        string date;
+        string fileName;
+        string sampleName;
+        float samplingRate;
+        float sampleLength;
+        float sampleWidth;
+
+    }
+}
---
+++ FinalSprint/MainWindow.xaml.cs
@@ -0,0 +1,313 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Syncfusion.SfSkinManager;
+using NationalInstruments.NI4882;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace FinalSprint
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Device device;
+
+        private int range;
+        private string rate;
+        private string compLevel;
+        private string currLevel;
+        private string out_put;
+
+        private CancellationTokenSource _canceller;
+
+        #region Fields
+        private string currentVisualStyle;
+        private string currentSizeMode;
+        #endregion
+
+        #region Properties
+        /// <summary>
+        /// Gets or sets the current visual style.
+        /// </summary>
+        /// <value></value>
+        /// <remarks></remarks>
+        public string CurrentVisualStyle
+        {
+            get
+            {
+                return currentVisualStyle;
+            }
+            set
+            {
+                currentVisualStyle = value;
+                OnVisualStyleChanged();
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the current Size mode.
+        /// </summary>
+        /// <value></value>
+        /// <remarks></remarks>
+        public string CurrentSizeMode
+        {
+            get
+            {
+                return currentSizeMode;
+            }
+            set
+            {
+                currentSizeMode = value;
+                OnSizeModeChanged();
+            }
+        }
+        #endregion
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.Loaded += OnLoaded;
+        }
+        /// <summary>
+        /// Called when [loaded].
+        /// </summary>
+        /// <param name="sender">The sender.</param>
+        /// <param name="e">The <see cref="RoutedEventArgs"/> instance containing the event data.</param>
+        private void OnLoaded(object sender, RoutedEventArgs e)
+        {
+            CurrentVisualStyle = "Windows11Light";
+            CurrentSizeMode = "Default";
+        }
+        /// <summary>
+        /// On Visual Style Changed.
+        /// </summary>
+        /// <remarks></remarks>
+        private void OnVisualStyleChanged()
+        {
+            VisualStyles visualStyle = VisualStyles.Default;
+            Enum.TryParse(CurrentVisualStyle, out visualStyle);
+            if (visualStyle != VisualStyles.Default)
+            {
+                SfSkinManager.ApplyStylesOnApplication = true;
+                SfSkinManager.SetVisualStyle(this, visualStyle);
+                SfSkinManager.ApplyStylesOnApplication = false;
+            }
+        }
+
+        /// <summary>
+        /// On Size Mode Changed event.
+        /// </summary>
+        /// <remarks></remarks>
+        private void OnSizeModeChanged()
+        {
+            SizeMode sizeMode = SizeMode.Default;
+            Enum.TryParse(CurrentSizeMode, out sizeMode);
+            if (sizeMode != SizeMode.Default)
+            {
+                SfSkinManager.ApplyStylesOnApplication = true;
+                SfSkinManager.SetSizeMode(this, sizeMode);
+                SfSkinManager.ApplyStylesOnApplication = false;
+            }
+        }
+
+        private void currRadio(object sender, RoutedEventArgs e)
+        {
+            // call SCPI connect to 6220
+            if (device != null)
+            {
+                device.Dispose();
+            }
+
+            int currentSecondaryAddress = 0;
+
+            device = new Device(0, 12, (byte)currentSecondaryAddress);
+
+            device.Write("*RST");
+            device.Write("CLE");
+        }
+
+        private void voltRadio(object sender, RoutedEventArgs e)
+        {
+            // call SCPI connect to 2182A
+            if (device != null)
+            {
+                device.Dispose();
+            }
+
+            int currentSecondaryAddress = 0;
+
+            device = new Device(0, 7, (byte)currentSecondaryAddress);
+
+            device.Write("*RST");
+            device.Write("INIT:CONT ON");
+        }
+
+        private void currText(object sender, TextChangedEventArgs e)
+        {
+            currLevel = currTextBox.Text;
+        }
+
+        private void compText(object sender, TextChangedEventArgs e)
+        {
+            compLevel = compTextBox.Text;
+        }
+
+        private void RangeDrop(object sender, SelectionChangedEventArgs e)
+        {
+            if (Range.SelectedIndex == 0)
+            {
+                range = 0;
+            }
+
+            if (Range.SelectedIndex == 1)
+            {
+                range = 1;
+            }
+
+            if (Range.SelectedIndex == 2)
+            {
+                range = 2;
+            }
+
+            if (Range.SelectedIndex == 3)
+            {
+                range = 3;
+            }
+        }
+
+        private void setCurr(object sender, RoutedEventArgs e)
+        {
+            if (range == 0)
+            {
+                /// SCPI COMMAND CURR:RANG:AUTO ON
+                try
+                {
+                    device.Write("CURR:RANG:AUTO ON");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+            else if (range == 1)
+            {
+                /// SCPI COMMAND CURR:RANG:1e-3
+                try
+                {
+                    device.Write("CURR:RANG:1e-3");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+            else if (range == 2)
+            {
+                /// SCPI COMMAND CURR:RANG:10e-3
+                try
+                {
+                    device.Write("CURR:RANG:10e-3");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+            else if (range == 3)
+            {
+                /// SCPI COMMAND CURR:RANG:100e-3
+                try
+                {
+                    device.Write("CURR:RANG:100e-3");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+
+            device.Write("CURR:COMP " + compLevel);
+            device.Write("CURR " + currLevel + "e-3");
+        }
+
+        private void currTurnON(object sender, RoutedEventArgs e)
+        {
+            device.Write("OUTP ON");
+        }
+
+        private void currTurnOFF(object sender, RoutedEventArgs e)
+        {
+            device.Write("OUTP OFF");
+        }
+
+        private void Integration_Rate_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (Integration_Rate.SelectedIndex == 0)
+            {
+                rate = "0.01";
+            }
+
+            if (Integration_Rate.SelectedIndex == 1)
+            {
+                rate = "0.1";
+            }
+
+            if (Integration_Rate.SelectedIndex == 2)
+            {
+                rate = "1";
+            }
+
+            if (Integration_Rate.SelectedIndex == 3)
+            {
+                rate = "5";
+            }
+        }
+
+        private void setVolt(object sender, RoutedEventArgs e)
+        {
+            device.Write("SENS:VOLT:NPLC " + rate);
+        }
+
+        private async void startCap(object sender, RoutedEventArgs e)
+        {
+            //startCapBtn.Enabled = false;
+            //stopCapBtn.Enabled = true;
+
+            _canceller = new CancellationTokenSource();
+            await Task.Run(() =>
+            {
+                do
+                {
+                    device.Write("FETC?");
+                    out_put = device.ReadString();
+                    //voltVals.Add(out_put);
+                    if (_canceller.Token.IsCancellationRequested)
+                        break;
+
+                } while (true);
+            });
+
+            _canceller.Dispose();
+            //startCapBtn.Enabled = true;
+            //stopCapBtn.Enabled = false;
+        }
+
+        private void stopCap(object sender, RoutedEventArgs e)
+        {
+            _canceller.Cancel();
+        }
+    }
+
+}
