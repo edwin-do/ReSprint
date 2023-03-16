7c745c8dceb71589acbbae21cd1b5d4acd971e7d
magarelt <magarelt@mcmaster.ca>
2023-03-16 13:56:45 -0400

    graph draft

---
+++ FinalSprint/Display/GraphicalOutput.cs
@@ -0,0 +1,82 @@
+﻿namespace ReSprint
+{
+    using Syncfusion.UI.Xaml.Charts;
+    using System;
+
+    public class GraphicalOutput
+    {
+        private readonly string hardwareInputHeader = "Time, Voltage, Current, Resistance, Resistivity, Temperture\n";
+        public void GraphInit()
+        {
+            SfChart graph = new SfChart();
+            CategoryAxis primaryAxis = new CategoryAxis();
+            graph.PrimaryAxis = primaryAxis;
+            NumericalAxis secondaryAxis = new NumericalAxis();
+            graph.SecondaryAxis = secondaryAxis;
+        }
+        
+        public void GraphTimeVResistivity(HardwareInput hardwareInput)
+        {
+            if (string.IsNullOrEmpty(hardwareInput.Time))
+                throw new ArgumentOutOfRangeException("hardwareInput.Time", "Value cannot be empty.");
+            if (hardwareInput.Resistivity < 0)
+                throw new ArgumentOutOfRangeException("hardwareInput.Resistivity", "Value cannot be negative.");
+            hardwareInput.Resistivity;
+            hardwareInput.Time;
+            graph.Header = "Time V Resistivity";
+        }
+
+        public void GraphTimeVResistance(HardwareInput hardwareInput)
+        {
+            if (string.IsNullOrEmpty(hardwareInput.Time))
+                throw new ArgumentOutOfRangeException("hardwareInput.Time", "Value cannot be empty.");
+            if (hardwareInput.Resistance < 0)
+                throw new ArgumentOutOfRangeException("hardwareInput.Resistance", "Value cannot be negative.");
+            hardwareInput.Resistance;
+            hardwareInput.Time;
+            graph.Header = "Time V Resistance";
+        }
+
+        public void GraphVoltageVResistance(HardwareInput hardwareInput)
+        {
+            if (hardwareInput.Voltage < 0)
+                throw new ArgumentOutOfRangeException("hardwareInput.Voltage", "Value cannot be negative.");
+            if (hardwareInput.Resistance < 0)
+                throw new ArgumentOutOfRangeException("hardwareInput.Resistance", "Value cannot be negative.");
+            hardwareInput.Resistance;
+            hardwareInput.Voltage;
+            graph.Header = "Voltage V Resistance";
+        }
+        public void GraphVoltageVResistivity(HardwareInput hardwareInput)
+        {
+            if (hardwareInput.Voltage < 0)
+                throw new ArgumentOutOfRangeException("hardwareInput.Voltage", "Value cannot be negative.");
+            if (hardwareInput.Resistivity < 0)
+                throw new ArgumentOutOfRangeException("hardwareInput.Resistivity", "Value cannot be negative.");
+            hardwareInput.Resistivity;
+            hardwareInput.Voltage;
+            graph.Header = "Voltage V Resistivity";
+        }
+        public void GraphTemperatureVResistance(HardwareInput hardwareInput)
+        {
+            if (string.IsNullOrEmpty(hardwareInput.Temperature))
+                throw new ArgumentOutOfRangeException("hardwareInput.Temperature", "Value cannot be empty.");
+            if (hardwareInput.Resistance < 0)
+                throw new ArgumentOutOfRangeException("hardwareInput.Resistance", "Value cannot be negative.");
+            hardwareInput.Resistance;
+            hardwareInput.Temperature;
+            graph.Header = "Temperature V Resistance";
+        }
+        public void GraphTemperatureVResistivity(HardwareInput hardwareInput)
+        {
+            if (string.IsNullOrEmpty(hardwareInput.Temperature))
+                throw new ArgumentOutOfRangeException("hardwareInput.Temperature", "Value cannot be empty.");
+            if (hardwareInput.Resistivity < 0)
+                throw new ArgumentOutOfRangeException("hardwareInput.Resistivity", "Value cannot be negative.");
+            hardwareInput.Resistivity;
+            hardwareInput.Temperature;
+            graph.Header = "Temperature V Resistivity";
+        }
+    }
+
+}
